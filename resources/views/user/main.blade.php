aa970862f91b25349bdaa90f89292e80bc378a72
Raldienpr325 <aldinprayoga26@webmail.umm.ac.id>
2021-11-01 21:22:31 +0700

    aldin commit

---
+++ resources/views/user/main.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PRESMA AND DPM FKUB</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/fontawesome-all.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <section class="services d-flex align-items-center py-5" id="services">
+        <div class="container text-light">
+            <div class="text-center pb-4">
+                <p>WEB PRESMA FKUB 2021/2022</p>
+                <h2 class="py-2">Universitas Brawijaya Malang</h2>
+                <p class="para-light">Web Masi Diperjuangkan Oleh pejuang Rupiah .... </p>
+                <br>
+                <button type="button" class="btn btn-primary">Login</button>
+            </div>
+        </div>
+    </section>
+
+    <section class="plans d-flex align-items-center py-5" id="plans">
+        <div class="container text-light">
+            <div class="text-center pb-4">
+                <p>Web DPM FKUB</p>
+                <h2 class="py-2">Universitas Brawijaya Malang</h2>
+                <p class="para-light">Web Masi Diperjuangkan Oleh pejuang Rupiah .... </p>
+                <br>
+                <button type="button" class="btn btn-primary">Login</button>
+            </div>
+
+        </div>
+    </section>
+</body>
+
+</html>
